8c94b8c18e09cc4f8a5e9b3d87b065e09bd8f202
ronnizan <ronnizan01@gmail.com>
2020-04-19 02:37:52 +0300

    style finished

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import NumberDisplay from '../number-display/number-display';
+import { genrateCells } from '../../utills/index';
+import Button from '../button/Button';
+
 
 const App: React.FC = () => {
+
+    const [cells, setCells] = useState(genrateCells());
+
+    const renderCells = (): React.ReactNode => {
+        return cells.map((row, rowIndex) => {
+           return row.map((cell, colIndex) => {
+               return <Button key={`${rowIndex}-${colIndex}`} />
+            })
+        })
+    }
+
+
     return (
         <div className="App">
             <div className="Header">
@@ -12,7 +27,7 @@ const App: React.FC = () => {
                 </div>
                 <NumberDisplay value={23} />
             </div>
-            <div className="Body">body</div>
+            <div className="Body">{renderCells()}</div>
         </div>
     )
 }

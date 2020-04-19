0314495dae3a84fdc4ef2242cbdc4dcc48495412
ronnizan <ronnizan01@gmail.com>
2020-04-19 22:09:25 +0300

    Updates

---
+++ precache-manifest.7a5382bb5e77428407b1fb62632f9f8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0271218c1f4cc4e0d0f33b7613aaca2e",
+    "url": "/minesweeper-react-ts/index.html"
+  },
+  {
+    "revision": "eb37dacbb53618210396",
+    "url": "/minesweeper-react-ts/static/css/main.93eddc51.chunk.css"
+  },
+  {
+    "revision": "51a6d1c86a777bb54d4a",
+    "url": "/minesweeper-react-ts/static/js/2.af1ca94e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/minesweeper-react-ts/static/js/2.af1ca94e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb37dacbb53618210396",
+    "url": "/minesweeper-react-ts/static/js/main.875db678.chunk.js"
+  },
+  {
+    "revision": "913c58e607939233594c",
+    "url": "/minesweeper-react-ts/static/js/runtime-main.8a0c0c6d.js"
+  }
+]);
